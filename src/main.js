896f1539a14a08ba527c510c426740309271e286
徐强 <1379577123@qq.com>
2019-12-27 09:17:06 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
+function addF (type = "ddddd") {
+  let a = ["1", "2", "3", "4"];
+  window.console.log(...a);
+  window.console.log(type);
+}
+function dc(){
+  return ["1", "3", "ww", "4", "5"];
+}
+addF();
+window.console.log(...dc());
+
 new Vue({
   router,
   store,
